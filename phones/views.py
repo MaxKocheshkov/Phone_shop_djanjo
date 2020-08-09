3c124779e0f1dc1095cb3f33a5b4692f23fa34cd
Maxim Kocheshkov <gror_godfroy@mail.ru>
2020-08-09 17:03:54 +0300

    версия 1.2

--- phones/views.py
+++ phones/views.py
@@ -2,28 +2,26 @@ from django.shortcuts import render
 from slugify import slugify
 from .models import Phone
 
-phone_data = Phone.objects.all()
-
 
 def show_catalog(request):
     template = 'catalog.html'
-    if 'name' in request.GET.get('sort'):
+    if request.GET.get('sort', 'name'):
         n_context = {'phone_data': Phone.objects.all().order_by("name")}
         return render(request, template, context=n_context)
-    elif 'min_price' in request.GET.get('sort'):
+    elif request.GET.get('sort', 'min_price'):
         min_context = {'phone_data': Phone.objects.all().order_by("price")}
         return render(request, template, context=min_context)
-    elif 'max_price' in request.GET.get('sort'):
+    elif request.GET.get('sort', 'max_price'):
         max_context = {'phone_data': Phone.objects.all().order_by("-price")}
         return render(request, template, context=max_context)
     else:
-        context = {'phone_data': phone_data}
+        context = {'phone_data': Phone.objects.all()}
         return render(request, template, context=context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    for name in Phone.objects.values_list("name", flat=True):
+    for name in Phone.objects.values("name", flat=True):
         slug = slugify(name)
-        context = {'slug': slug, 'phone_data': phone_data}
+        context = {'slug': slug, 'phone_data': Phone.objects.all()}
         return render(request, template, context=context)
